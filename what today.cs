8d5e03cfde79d0b7ea1eefb1cae0d0317e88138b
VladimirPetrosyan <92800312+VladimirPetrosyan@users.noreply.github.com>
2021-10-27 13:23:25 +0300

    Add files via upload

---
+++ what today.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace project
+{
+    class program
+    {
+        static void Main()
+        {
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1)
+            {
+                Console.WriteLine("Понедельник");
+                if (a == 2)
+                {
+                    Console.WriteLine("Вторник");
+                    if (a == 3)
+                    {
+                        Console.WriteLine("Среда");
+                        if (a == 4)
+                        {
+                            Console.WriteLine("Четверг");
+                            if (a == 5)
+                            {
+                                Console.WriteLine("Пятница");
+                                if (a == 6)
+                                {
+                                    Console.WriteLine("Суббота");
+                                    if (a == 7)
+                                    {
+                                        Console.WriteLine("Воскресенье");
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (a < 1 || a > 7)
+            {
+                Console.WriteLine("Думаю вы ошиблись с числом, число должно быть от 1 и до 7,пожалуйста повторите попытку");
+            }
+        }
+    }
+}
